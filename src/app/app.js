0970ab7f19597f4e7c094a29380161480374b470
Esko Cruzeta <eskocruz@users.noreply.github.com>
2015-09-01 11:24:15 -0700

    Initial Commit

---
+++ src/app/app.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+    var app = angular.module('sportsAdmin', [
+        // Angular modules
+        'ngRoute',
+
+        // 3rd Party Modules
+        'ui.bootstrap'
+    ]);
+
+    app.config(['$routeProvider', configRoutes]);
+
+    function configRoutes($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/home/home.html',
+                controller: 'HomeCtrl',
+                controllerAs: 'vm'
+            });
+
+
+        $routeProvider.otherwise('/');
+    }
+
+    app.run(['$route', function ($route) {
+        // Include $route to kick start the router.
+    }]);
+})();
